ab3016c3126f1a26e3a742c9228a84ad48b3eff5
sandeepa <sanvidurath98@gmail.com>
2025-01-23 16:57:45 +0530

    third commit to fixing nav-link issue

--- scripts/app.js
+++ scripts/app.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
   const currentUrl = window.location.href;
   navLinkElements.forEach((navLink) => {
     if (currentUrl.includes(navLink.href)) {
-      //navLink.classList.add("navbar-active");
+      navLink.classList.add("navbar-active");
     } else {
       navLink.classList.remove("navbar-active");
     }
