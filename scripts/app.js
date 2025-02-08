717ec7632f5f3a51422bec6fd6bdb1f1bedab933
sandeepa <sanvidurath98@gmail.com>
2025-02-08 10:19:06 +0530

    finishing items

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,10 @@
 import items from "./items.js";
+const selectedCategory = document.querySelector(".itemCategories");
 
 document.addEventListener("DOMContentLoaded", (event) => {
   loadNavBar();
   setActiveNavLink();
+  loadItems();
 });
 
 function setActiveNavLink() {
@@ -11,10 +13,10 @@ function setActiveNavLink() {
   loadNavLinkActive(navLinkElements, currentUrl);
 }
 
-function loadNavLinkActive(navLinkElements, currentUrl){
-  if(currentUrl==="https://sanvidurath.github.io/Burger-Shop/"){
+function loadNavLinkActive(navLinkElements, currentUrl) {
+  if (currentUrl === "https://sanvidurath.github.io/Burger-Shop/") {
     document.getElementById("home").classList.add("navbar-active");
-  }else{
+  } else {
     navLinkElements.forEach((navLink) => {
       if (currentUrl.includes(navLink.href)) {
         navLink.classList.add("navbar-active");
@@ -74,21 +76,23 @@ function loginPage() {
   Swal.fire("Under construction!");
 }
 
-items.forEach((item) => {
-  document.getElementById("item-cards").innerHTML += `
-  <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 my-2">
-            <div class="card h-100">
-              <img src=${selectImage(
-                item.itemtype
-              )} class="card-img-top" alt="food" />
-              <div class="card-body text-end">
-                <h5 class="card-title text-center">${item.itemname}</h5>
-                <p class="card-text text-center">Rs.${item.price}</p>
-                <a href="#" class="btn btn-success"><i class="fa-solid fa-cart-shopping">+</i></a>
+function loadItems() {
+  items.forEach((item) => {
+    document.getElementById("item-cards").innerHTML += `
+    <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 my-2">
+              <div class="card h-100">
+                <img src=${selectImage(
+                  item.itemtype
+                )} class="card-img-top" alt="food" />
+                <div class="card-body text-end">
+                  <h5 class="card-title text-center">${item.itemname}</h5>
+                  <p class="card-text text-center">Rs.${item.price}</p>
+                  <a href="#" class="btn btn-success"><i class="fa-solid fa-cart-shopping">+</i></a>
+                </div>
               </div>
-            </div>
-          </div>`;
-});
+            </div>`;
+  });
+}
 
 function selectImage(itemtype) {
   switch (itemtype) {
@@ -105,4 +109,79 @@ function selectImage(itemtype) {
     case "Drinks":
       return "./images/beverages-2.png";
   }
+}
+
+selectedCategory.addEventListener("change", getSelectedValue);
+
+function getSelectedValue() {
+  document.getElementById("item-cards").innerHTML = "";
+  var itemCategory =
+    selectedCategory.options[selectedCategory.selectedIndex].text;
+  switch (itemCategory) {
+    case "Burgers":
+      items
+        .filter((item) => item.itemtype === "Burger")
+        .forEach((burgerItem) => {
+          setInnerHtml(burgerItem);
+        });
+      break;
+    case "Submarines":
+      items
+        .filter((item) => item.itemtype === "Submarine")
+        .forEach((submarineItem) => {
+          setInnerHtml(submarineItem);
+        });
+      break;
+    case "Fries":
+      items
+        .filter((item) => item.itemtype === "Fries")
+        .forEach((friesItem) => {
+          setInnerHtml(friesItem);
+        });
+      break;
+    case "Pasta":
+      items
+        .filter((item) => item.itemtype === "Pasta")
+        .forEach((pastaItem) => {
+          setInnerHtml(pastaItem);
+        });
+      break;
+    case "Chicken":
+      items
+        .filter((item) => item.itemtype === "Chicken")
+        .forEach((chickenItem) => {
+          setInnerHtml(chickenItem);
+        });
+      break;
+    case "Drinks":
+      items
+        .filter((item) => item.itemtype === "Drinks")
+        .forEach((drinkItem) => {
+          setInnerHtml(drinkItem);
+        });
+      break;
+    case "All":
+      loadItems();
+      break;
+  }
+}
+
+function setInnerHtml(itemCategory) {
+  document.getElementById("item-cards").innerHTML += `
+        <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 my-2">
+                  <div class="card h-100">
+                    <img src=${selectImage(
+                      itemCategory.itemtype
+                    )} class="card-img-top" alt="food" />
+                    <div class="card-body text-end">
+                      <h5 class="card-title text-center">${
+                        itemCategory.itemname
+                      }</h5>
+                      <p class="card-text text-center">Rs.${
+                        itemCategory.price
+                      }</p>
+                      <a href="#" class="btn btn-success"><i class="fa-solid fa-cart-shopping">+</i></a>
+                    </div>
+                  </div>
+                </div>`;
 }
